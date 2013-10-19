b1e77db7c1eb8d354ec7c4a010026e8c8034e084
peggy110336 <peggy110336@gamil.com>
2013-10-19 14:48:05 +0800

    include javascript,fix #8

---
+++ main.js
@@ -0,0 +1,17 @@
+
+(function()
+{
+	var canvas = document.getElementById('game'),
+		ctx = canvas.getContext('2d'),
+		ballx = Math.floor(Math.random() * 300),
+		bally = Math.floor(Math.random() * 500),
+		ballr = Math.floor(Math.random() * 100);
+
+	canvas.width=480;
+	canvas.height=320;
+
+	ctx.fillStyle='Black';
+	ctx.beginPath();
+	ctx.arc(ballx,bally,ballr,0,Math.PI * 2,true);
+	ctx.fill();
+})  ();
