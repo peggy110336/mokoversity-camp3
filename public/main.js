8bb3638ccf82c7b93f0540387c7524010e7a06ba
peggy110336 <peggy110336@gamil.com>
2013-10-20 12:46:09 +0800

    fix #15
    通過嚴謹與法檢測了!!

---
+++ public/main.js
@@ -0,0 +1,72 @@
+var gameModule =(function(){
+
+	var timeoutover,
+	    counter=0,
+	    ballx,
+	    bally,
+	    ballr,
+	    Scores=0,
+		colors = ['#ff0000', '#0000ff', 'yellow'],
+   		length = colors.length;
+
+	function touchEvent(evt){
+		var X = evt.clientX;
+			Y = evt.clientY;
+			tmp = (ballx - X)*(ballx - X)+(bally-Y)*(bally-Y);
+
+		console.log("Clicked : "+X+" , "+Y);
+
+		if(tmp<ballr*ballr){
+			Scores = Scores + (180-ballr);
+			console.log("Good Hit! Your Scores : "+Scores);
+		}
+	}
+
+	function start(){
+		document.getElementById("main").addEventListener("click",touchEvent,false);
+		startgame();
+	}
+
+	function startgame(){
+	 
+		var canvas = document.getElementById('game'),
+			ctx = canvas.getContext('2d');
+			
+			ballx = Math.floor(Math.random() * 300);
+			bally = Math.floor(Math.random() * 500);
+			ballr = Math.floor(Math.random() * 100);
+
+		canvas.width=480;
+		canvas.height=320;
+
+		ctx.fillStyle=colors[counter % length];
+		ctx.beginPath();
+		ctx.arc(ballx,bally,ballr,0,Math.PI * 2,true);
+		ctx.fill();
+
+		if(counter>=10){
+				gameover();
+		}
+		else{
+				timeoutover = setTimeout(startgame,1000);
+				counter = counter+1;
+				
+			}
+	 }
+
+	 function gameover(){
+
+	 		console.log("Final Scores = "+Scores);
+	 }
+
+	 return{
+				start : start
+				
+		}
+	
+})();
+
+gameModule.start();
+
+
+
