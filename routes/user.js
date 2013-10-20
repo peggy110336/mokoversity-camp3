4eef42cbc4c29728a54d5e156468e89964aabb89
peggy110336 <peggy110336@gamil.com>
2013-10-20 14:54:31 +0800

    fix #16

---
+++ routes/user.js
@@ -0,0 +1,15 @@
+
+/*
+ * GET users listing.
+ */
+
+var score = [];
+
+exports.list = function(req, res){
+  res.send("respond with a resource");
+};
+
+exports.save = function(req, res){
+  scpre.push(reg.query.s);
+  console.log("Your Score : " + score);
+};
